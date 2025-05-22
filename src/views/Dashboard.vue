be738f5861af6ab9e2d4833fbd4f5a0330d65e22
Luís Edu <luisedu.prog@gmail.com>
2025-05-22 19:35:20 -0300

    teste Luís

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,23 +1,22 @@
 <template>
-    <h1>mudando isso aqui</h1>
+  <h1>mudando isso aqui</h1>
 </template>
 
 <script>
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 
 export default {
-  name: 'Dashboard',
+  name: "Dashboard", //teste Luís
   created() {
     const auth = getAuth();
     const user = auth.currentUser;
 
     // Se o usuário não estiver logado, redireciona para a página de login
     if (!user) {
-      this.$router.push('/Login');
+      this.$router.push("/Login");
     }
   },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
