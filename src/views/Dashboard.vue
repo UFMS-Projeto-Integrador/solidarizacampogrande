18aa42729dfb0aed56ca75a46ed5d5422eea97dc
HenriqueJP-Tech <henriquepereiraemp@gmail.com>
2025-05-22 19:38:34 -0400

    Atualizando escopo dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,22 +1,34 @@
 <template>
-  <h1>mudando isso aqui</h1>
+    <div id="navigation">
+      <div id="profile"></div>
+      <div id="navigations"> </div>
+    </div>
+    <div id="Search">
+      <div id="input-search"></div>
+      <div id="button-search"></div>
+    </div>
+    <div id="Crud">
+
+    </div>
 </template>
 
 <script>
-import { getAuth } from "firebase/auth";
+import { getAuth } from 'firebase/auth';
 
 export default {
-  name: "Dashboard", //teste Luís
+  name: 'Dashboard',
   created() {
     const auth = getAuth();
     const user = auth.currentUser;
 
     // Se o usuário não estiver logado, redireciona para a página de login
     if (!user) {
-      this.$router.push("/Login");
+      this.$router.push('/Login');
     }
   },
 };
 </script>
 
-<style scoped></style>
+<style>
+
+</style>
